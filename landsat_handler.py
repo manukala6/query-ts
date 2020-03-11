d0658e04834245d5429591c5153aedc57f9837a2
Maanas Nukala <manukala6@gmail.com>
2020-03-10 22:20:54 -0400

    updated querying

--- landsat_handler.py
+++ landsat_handler.py
@@ -3,12 +3,34 @@
 import json
 import boto3
 
-def landsat_handler(event, context):
+event = {
+  "queryStringParameters": {
+    "path": "143",
+    "row": "37"
+  }
+}
+
+def bad_request():
+    error = {}
+    error['Message'] = 'Bad request'
+    badResponse = {}
+    badResponse['statusCode'] = 200
+    badResponse['headers'] = {}
+    badResponse['headers']['Content-Type'] = 'application/json'
+    badResponse['body'] = json.dumps(error)
+
+    return badResponse
+
+def landsat_handler(event):
 
     # parse event
-    path = event['path']
-    row = event['row']
-    
+    path = event['queryStringParameters']['path']
+    row = event['queryStringParameters']['row']
+    path = path.zfill(3)
+    row = row.zfill(3)
+    if int(path) > 233 or int(row) > 233:
+        return bad_request()
+
     # search landsat s3 bucket
     s3 = boto3.client('s3', region_name='us-west-2')
     params = {
@@ -18,7 +40,10 @@ def landsat_handler(event, context):
     }
     dirs = []
     s3Response = s3.list_objects_v2(**params)
-    prefixes = s3Response['CommonPrefixes']
+    try:
+        prefixes = s3Response['CommonPrefixes']
+    except:
+        return bad_request()
     for prefix in prefixes:
         raw_dir = list(prefix.values())
         dirs.append(raw_dir[0])
@@ -37,6 +62,10 @@ def landsat_handler(event, context):
 
     # return
     return responseObject
+
+responseObject = add_zero(path)
+print(responseObject)
+
 
 '''
 
